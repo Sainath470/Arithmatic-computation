6bcdfb3bd39e4361dc8380561df8659e68e1b242
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-02 02:57:32 +0530

    Added:Dictory for every computation

--- arithmaticCoumputation.sh
+++ arithmaticCoumputation.sh
@@ -1,5 +1,16 @@
 echo "Enter the three inputs: "
 read a b c
 
-result5=$(($a%$b+$c))
-echo $result5
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
+
+declare -A ar
+
+ar[comp1]=$result1
+ar[comp2]=$result2
+ar[comp3]=$result3
+ar[comp4]=$result4
+
+echo ${ar[@]}
